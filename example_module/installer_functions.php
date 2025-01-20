dd835ac7d8750b5087bd8b31a269a692a6f8f8e3
ParkerFamilyDevelopment <noahjayparker@gmail.com>
2025-01-20 16:57:51 -0500

    Add files via upload

---
+++ example_module/installer_functions.php
@@ -0,0 +1,4 @@
+<?php
+//This file can be used in the future to perform actions such as database additions/alterations upon installation.
+echo "If you can see this the installer functions is working.";
+?>
